771cece1fd5379ebd533b97af5af0b07acc2184a
rouz majlessi <rmajlessi@gmail.com>
2016-12-16 14:27:12 -0800

    set up initial files and folders

---
+++ src/portfolioApi/Startup.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
+
+namespace portfolioApi
+{
+    public class Startup
+    {
+
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json");
+
+        }
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+
+        }
+
+
+        public void Configure(IApplicationBuilder app)
+        {
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+
+            app.UseStaticFiles();
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Error!");
+            });
+        }
+    }
+}
